4666b9916950ea568690fb5e285cb67e37164857
Giotreglia <giovanni.treglia@gmail.com>
2023-05-31 16:17:54 +0200

    modificato navbar aggiunti router link

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,24 @@
 <script>
 export default {
-    name: 'TheHeader'
+    name: 'TheHeader',
+    data() {
+        return {
+            menuItems: [
+                {
+                    label: 'Home',
+                    routeName: 'home'
+                },
+                {
+                    label: 'Contatti',
+                    routeName: 'contacts'
+                },
+                {
+                    label: 'Progetti',
+                    routeName: 'projects'
+                },
+            ]
+        }
+    },
 }
 </script>
 
@@ -26,28 +44,9 @@ export default {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                        <li class="nav-item" v-for="item in menuItems">
+                            <router-link :to="{ name: item.routeName }" class="nav-link active" aria-current="page"
+                                href="#">{{ item.label }}</router-link>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ const router = createRouter({
         },
         {
             path: '/contatti',
-            name: 'contact',
+            name: 'contacts',
             component: AppContacts
         },
         {
